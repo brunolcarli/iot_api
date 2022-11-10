e1c4537c1f83209c44f26ae730c65417f98f7f8a
Bruno L. Carli <brunolcarli@gmail.com>
2022-11-10 20:37:41 -0300

    ADD basic esp tx query

--- greenhouse/schema.py
+++ greenhouse/schema.py
@@ -1,4 +1,15 @@
 import graphene
+from greenhouse.models import ESPTransmission
+
+
+class ESPTransmissionType(graphene.ObjectType):
+    timestamp_origin = graphene.Int()
+    timestamp_receive = graphene.Int()
+    mac_address = graphene.String()
+    ldr_sensor = graphene.Float()
+    temperature_sensor = graphene.Float()
+    pressure = graphene.Float()
+    moisture = graphene.Float()
 
 
 class Query(graphene.ObjectType):
@@ -6,3 +17,10 @@ class Query(graphene.ObjectType):
 
     def resolve_version(self, info, **kwargs):
         return '0.0.0'
+
+    esp_transmissions = graphene.List(
+        ESPTransmissionType
+    )
+
+    def resolve_esp_transmissions(self, info, **kwargs):
+        return ESPTransmission.objects.Filter(**kwargs)
