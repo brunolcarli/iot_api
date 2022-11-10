043ef9229089e9746c3d13c40e7d2b2210bdb4f0
Bruno L. Carli <brunolcarli@gmail.com>
2022-11-10 20:09:59 -0300

    CHAGE get secret key from env var

--- iot_api/settings.py
+++ iot_api/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'js1d^_!hleflyu+l4-fle&_#^r$t%&n=jgb9!_vpje0@mt_47v'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
