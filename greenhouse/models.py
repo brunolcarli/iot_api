7b587788189409b11b1ccc79f916bcd8b5f52171
Bruno L. Carli <brunolcarli@gmail.com>
2022-11-10 20:32:37 -0300

    ADD esp transmission model

--- greenhouse/models.py
+++ greenhouse/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class ESPTransmission(models.Model):
+    timestamp_origin = models.IntegerField(null=False)
+    timestamp_receive = models.IntegerField(null=False)
+    mac_address = models.CharField(max_length=50, null=False, blank=False)
+    ldr_sensor = models.FloatField()
+    temperature_sensor = models.FloatField()
+    pressure = models.FloatField()
+    moisture = models.FloatField()
