8ac49c9f14b9ebd779508a8b2678e4737b643d65
Bruno L. Carli <brunolcarli@gmail.com>
2022-12-08 03:22:32 -0300

    CHANGE version

--- greenhouse/schema.py
+++ greenhouse/schema.py
@@ -121,7 +121,7 @@ class Query(graphene.ObjectType):
     version = graphene.String()
 
     def resolve_version(self, info, **kwargs):
-        return '0.0.5'
+        return '0.0.6'
 
     esp_transmissions = graphene.List(
         ESPTransmissionType,
